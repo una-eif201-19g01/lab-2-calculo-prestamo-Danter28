a8714a856e124d7f31c7079c335132e09261f884
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-08-09 17:26:00 -0600

    main()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "CalculoPrestamo.h"
-
+using std::cin;
+using std::cout;
 int main() {
+    CalculoPrestamo calc1(30000.000000);
+    cout<<"Calcular prestamo:\n"<<calc1.reporteCalculoPrestamo("1A", 8.40%);
+    
+    
+    
+    cin.get();
     return 0;
 }
