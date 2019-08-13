0ae0012f8b98c56ef1ef24a3c99d03cd377475c5
ESCINF <ESCINF@LEN-M83>
2019-08-13 08:17:20 -0600

    correccion de reporte()5

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -63,6 +63,7 @@ string CalculoPrestamo::reporteCalculoPrestamo(string tiempo, string porcentaje)
                 std::to_string (balanceNuevo) + "]\n";
        
         balanceInicial = balanceNuevo;
+        interes = calcularInteresMensual(balanceInicial, (obtenerPorcentaje(porcentaje)));
         balanceNuevo = balanceInicial + interes;
     }
  return tasaTexto;
